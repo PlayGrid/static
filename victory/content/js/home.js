931cafa49aaa92a403f88b94d8bf003f164b8760
Daron Stinnett <darons@playgrid.com>
2014-10-17 15:17:41 -0700

    updated

---
+++ victory/content/js/home.js
@@ -0,0 +1,138 @@
+var $adminMenu = $('#navbar');
+var $menuFixed = $('#menu-fixed');
+var $applyModal = $('#apply-modal');
+var $hidden = true;
+var vimeoPlayer = $f($('#vimeoplayer')[0]);
+
+$(document).ready(function(){
+    // When the player is ready, add listeners for pause, finish, and playProgress
+
+    vimeoPlayer.addEvent('ready', function() {
+        $('.watch').show();
+        vimeoPlayer.addEvent('finish', onFinish);
+    });
+
+    // Call the API when a button is pressed
+    $('#video').on('click', function() {
+    	vimeoPlayer.api('play');
+    	if (typeof ga != 'undefined')
+    		ga('send', 'event', 'videos', 'play', 'Trailer01');
+    	$('#video').hide();
+    	$('#player-wrapper').show()
+    });
+
+    function onFinish(id) {
+        $('#video').show();
+        $('#player-wrapper').hide()
+    }
+	
+	
+    // handle top menu on scroll
+	$(window).scroll(function(){
+        if ($(this).scrollTop() > 200) {
+        	if($hidden){
+	        	clearTimeout($adminMenu.t);
+	        	$menuFixed.t = setTimeout((function() {
+	                $menuFixed.stop().fadeIn(500);
+	            }), 200);
+	            $adminMenu.stop().animate({
+	            	maxHeight: '85px',
+	            	height: '85px'
+	            }, 500);
+	            $hidden = false;
+            }
+        } else {
+        	if(!$hidden){
+	        	clearTimeout($menuFixed.t);
+	        	$adminMenu.t = setTimeout((function() {
+	        		$adminMenu.stop().animate({
+			        	maxHeight: '30px',
+		    	    	height: '30px'
+		        	}, 500);
+	        	}), 200)
+	            $menuFixed.stop().fadeOut(500);
+	            $hidden = true;
+	        }
+        }
+    });
+
+
+
+    $('.scroll').on('click', function(e){
+    	e.preventDefault();
+    	$('html, body').animate({
+        	scrollTop: $("#"+$(this).data('scroll')).offset().top - 100
+    	}, 200);
+    });
+    
+    // detect content height changes and post to parent
+    var prevHeight = $('#content').height();
+    var origin;
+    if(window.location.origin !== "file://") {
+        origin = window.location.origin;
+    } else {
+        origin = "*";
+    }
+    postMessage("content_height_change", prevHeight, origin);
+    $( window ).resize(function() {
+        var curHeight = $("#content").height();            
+        if (prevHeight !== curHeight) {
+            postMessage("content_height_change", curHeight, origin);
+            prevHeight = curHeight;
+        }            
+    });
+
+
+    // listen for messages from parent 
+    window.addEventListener( "message",
+        function (e) {
+            if(e.origin == "null" || e.origin == window.location.origin) {
+                var data = e.data.data;
+                switch(e.data.message) {
+                    case 'apply':
+                        $applyModal.modal('show');
+                        break;
+                }
+            }
+        }, 
+    false);
+
+
+
+    // Beta signup form ajax
+    $('#earlyAccess').submit(function (event) {
+        /* stop form from submitting normally */
+        event.preventDefault();
+
+        var url = getQueryParameter('apply_url');
+        if (url) {
+            var request = $.ajax({
+                type: "POST",
+                url: url,
+                data: {email: $('#email').val(), note: $('#note').val()},
+                headers: {Authorization: "PGP {{ request.game.untrusted_access_token }}"},
+                dataType: 'json'
+            });
+ 
+            request.done(function( msg ) {
+                $("#earlyAccess .errors").empty();
+                $('#apply-modal').modal('hide');
+                $('#messages .thanks').remove();
+                $('#messages').append('<div class="thanks alert alert-success" style="display:none" role="alert">Thanks for applying!</div>');
+                $('#messages .thanks').slideDown().delay(5000).slideUp();
+                ga('send', 'event', 'beta', 'signup', 'cta');
+            });
+     
+            request.fail(function( jqXHR, textStatus ) {
+                var message = "foo";
+                for(var key in jqXHR.responseJSON) {
+                    $("#earlyAccess .errors").append("<p>"+jqXHR.responseJSON[key]+"</p>");
+                }
+            });
+        } else {
+            $("#earlyAccess .errors").append("<p>beta form requires 'apply_url' provided as query param</p>");
+        }
+    });   
+})
+
+
---
+++ victory/content/js/utils.js
@@ -0,0 +1,11 @@
+
+function getQueryParameter(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+function postMessage(message, data, origin) {
+    parent.postMessage({message: message, data: data}, origin);
+}
