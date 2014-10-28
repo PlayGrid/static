27beb9b1eb01a972c74210e05a7fb4c04ffd47b9
Daron Stinnett <darons@playgrid.com>
2014-10-28 14:27:48 -0700

    updated apply modal logic work in index

--- victory/js/index.js
+++ victory/js/index.js
@@ -83,33 +83,30 @@ $(document).ready(function(){
         /* stop form from submitting normally */
         event.preventDefault();
 
-        var url = getQueryParameter('apply_url');
-        if (url) {
-            var request = $.ajax({
-                type: "POST",
-                url: url,
-                data: {email: $('#email').val(), note: $('#note').val()},
-                headers: {Authorization: "PGP {{ request.game.untrusted_access_token }}"},
-                dataType: 'json'
-            });
+        var url = getQueryParameter('apply_url') || "http://api.playgrid.com/api/2.1/players/apply/";
+        var token = $("#earlyAccess").attr("data-token");
+        var request = $.ajax({
+            type: "POST",
+            url: url,
+            data: {email: $('#email').val(), note: $('#note').val()},
+            headers: {Authorization: token},
+            dataType: 'json'
+        });
+
+        request.done(function( msg ) {
+            $("#earlyAccess .errors").empty();
+            $('#apply-modal').modal('hide');
+            $('#messages .thanks').remove();
+            $('#messages').append('<div class="thanks alert alert-success" style="display:none" role="alert">Thanks for applying!</div>');
+            $('#messages .thanks').slideDown().delay(5000).slideUp();
+            ga('send', 'event', 'beta', 'signup', 'cta');
+        });
  
-            request.done(function( msg ) {
-                $("#earlyAccess .errors").empty();
-                $('#apply-modal').modal('hide');
-                $('#messages .thanks').remove();
-                $('#messages').append('<div class="thanks alert alert-success" style="display:none" role="alert">Thanks for applying!</div>');
-                $('#messages .thanks').slideDown().delay(5000).slideUp();
-                ga('send', 'event', 'beta', 'signup', 'cta');
-            });
-     
-            request.fail(function( jqXHR, textStatus ) {
-                var message = "foo";
-                for(var key in jqXHR.responseJSON) {
-                    $("#earlyAccess .errors").append("<p>"+jqXHR.responseJSON[key]+"</p>");
-                }
-            });
-        } else {
-            $("#earlyAccess .errors").append("<p>beta form requires 'apply_url' provided as query param</p>");
-        }
+        request.fail(function( jqXHR, textStatus ) {
+            var message = "foo";
+            for(var key in jqXHR.responseJSON) {
+                $("#earlyAccess .errors").append("<p>"+jqXHR.responseJSON[key]+"</p>");
+            }
+        });
     });   
 });		
